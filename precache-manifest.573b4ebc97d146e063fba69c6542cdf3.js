758a42b1962512c644e9cc3c854094667ea5133d
Alex Pomazan <multdirectx@gmail.com>
2019-10-27 21:26:01 +0200

    Updates

---
+++ precache-manifest.573b4ebc97d146e063fba69c6542cdf3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d634e0aef1b2641b994ec7ee2618b3",
+    "url": "/cube/index.html"
+  },
+  {
+    "revision": "4e4f42bae1bb10b89e6a",
+    "url": "/cube/static/js/2.63f8e6b3.chunk.js"
+  },
+  {
+    "revision": "0069d16cb416d5b5d649",
+    "url": "/cube/static/js/main.40ba3622.chunk.js"
+  },
+  {
+    "revision": "b453615652fb509fac85",
+    "url": "/cube/static/js/runtime-main.e5259308.js"
+  }
+]);
